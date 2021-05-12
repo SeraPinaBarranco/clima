f1459f382792442b38bf90d0ea1844ef6028d881
Sera Pina Barranco <arespin23@gmail.com>
2021-05-12 22:41:32 +0200

    deploy

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ClimaService } from 'src/app/services/clima.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,12 +9,39 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   urlImagen= "https://image.flaticon.com/icons/png/512/1116/1116453.png";
   ciudad= "";
-  constructor() { }
+  temperatura=0;
+  humedad=0;
+  clima='';
+  query=false;
+  loading=false;
+  mostrarError= false;
+
+  constructor(private _clima: ClimaService) { }
 
   ngOnInit(): void {
   }
 
   obtenerClima(){
-    console.log(this.ciudad);
+    this.query=false;
+    this.loading= true;
+    this._clima.getClima(this.ciudad).subscribe(data => {
+      this.query=true;
+      this.loading=false;
+      this.temperatura= data.main.temp -273;
+      this.humedad= data.main.humidity;
+      this.clima= data.weather[0].main;
+
+    }, error=>{
+      this.loading=false;
+      this.error();
+    });
+
+  }
+  error(){
+    this.mostrarError=true;
+    setTimeout(() => {
+      this.mostrarError=false;
+      this.ciudad= '';
+    }, 3000);
   }
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +10,9 @@ export class ClimaService {
   key= 'f930f6925c7aa53fe294daeca53835fb';
 
   constructor(private http: HttpClient) { }
+
+  getClima(ciudad: string):Observable<any>{
+    let url= `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${this.key}`;
+    return this.http.get(url);
+  }
 }
