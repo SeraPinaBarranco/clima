2a23c580c6dfe6584514b07ed6dc3ac90dac82e9
SeraPinaBarranco <arespin23@gmail.com>
2021-05-12 14:41:21 +0200

    api Weather

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
+  urlImagen= "https://image.flaticon.com/icons/png/512/1116/1116453.png";
+  ciudad= "";
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  obtenerClima(){
+    console.log(this.ciudad);
+  }
 }
--- src/app/services/clima.service.ts
+++ src/app/services/clima.service.ts
@@ -1,9 +1,12 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
+  url='api.openweathermap.org/data/2.5/weather?&appid=';
+  key= 'f930f6925c7aa53fe294daeca53835fb';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 }
